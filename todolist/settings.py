044c1b9010ac585fc23e7ab162ee5efe385b2dbb
Sedir Morais <philippi.sedir@gmail.com>
2019-07-03 14:24:10 -0300

    Configuração de arquivos estáticos.

--- todolist/settings.py
+++ todolist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
+from pathlib import Path
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -129,5 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_ROOT = Path(BASE_DIR) / 'run' / 'static'
 
 STATIC_URL = '/static/'
